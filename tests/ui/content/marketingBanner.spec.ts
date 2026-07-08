214fb75d837712dd081e1cfa0741b43039576d47
Balaji Mudaliyar <34879806+balaji-mudaliyar@users.noreply.github.com>
2026-07-08 20:32:03 +0400

    Minor Fix (#23)

--- tests/ui/content/marketingBanner.spec.ts
+++ tests/ui/content/marketingBanner.spec.ts
@@ -3,7 +3,7 @@ import { LoginPage } from '../../../src/pages/navigation/LoginPage';
 
 test('should render the marketing banner in the expected page region', async ({ page }) => {
   const loginPage = new LoginPage(page);
-
+  
   await loginPage.open();
   await loginPage.expectMarketingBannerInRightPanel();
 });
