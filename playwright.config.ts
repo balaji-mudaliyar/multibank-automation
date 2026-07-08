018f4caf5dda037fda80912d64159beee2ce6e2b
Balaji Mudaliyar <34879806+balaji-mudaliyar@users.noreply.github.com>
2026-07-08 12:56:24 +0400

    Revert "Add github pages (#14)"
    This reverts commit 0676319228a916d2933708b5e2ab5ac5f5172a89.

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,14 +19,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
   ],
   outputDir: 'test-results/',
   snapshotPathTemplate: path.join('snapshots', '{testFilePath}', '{arg}{ext}'),
